7d8d97d90766d07bfe29aa38b8e005bdaca1a294
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-06-02 16:12:59 -0500

    add img

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { NavBar, Banner } from './components';
 
 const App = ()=>{
   return (
-    <>
+    <div className='app'>
       <NavBar />
       <Banner />
-    </>
+    </div>
   );
 }
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,32 @@
 import '../estilos/Banner.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { header720x720 } from '../Assets';
 
-const Banner = ({ contador })=>{
+const Banner = ()=>{
     return (
-      <div className='container-banner'>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Culpa maiores minima quasndis cum ducimus ex 
-          omnis quidem, aliquam deserunt commodi libero?</h1>
-      </div>
+      <section className='banner' id='home'>
+        <Container>
+          <Row className='align-items-center'>
+            <Col xs={12} md={6} xl={7}>
+              <span className='tagline'>Welcome to my Portfolio</span>
+              <h1>{`I'm webdecoded `}<span className='wrap'>web developer</span></h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis 
+                ratione pariatur rem. Magni ad dolorem numquam mollitia ducimus eos 
+                porro reiciendis, explicabo neque aut rem! Natus dicta molestiae 
+                autem veniam.
+              </p>
+             <button className='btn btn-outline-primary' onClick={()=> console.log('connect')}><span>Let's Connect <BsArrowRightCircle size={25} /></span></button>
+            </Col>
+            <Col>
+              <div className='imgHeader'>
+                <img src={header720x720} alt="Headder" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     )
 }
 
